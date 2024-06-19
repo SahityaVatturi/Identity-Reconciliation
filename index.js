007d012df3e3a9008de6dfebdb533803753b0c10
SahithyaVatturi <sahithyavatturi28@gmail.com>
2024-06-19 15:10:01 +0530

    resolved vercel deployment issue

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ try {
     res.status(200).send("Server is running");
   });
 
-  // app.listen(process.env.PORT, () => {
-  //   console.log(`Server running on port ${process.env.PORT}`);
-  // });
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
 } catch (error) {
   console.log(error);
   throw new Error(error);
 }
 
-module.exports = app;
+// module.exports = app;
--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const app = require("../../app.js");
+const app = require("../../index.js");
 require("dotenv").config();
 
 app.listen(process.env.PORT, () => {
