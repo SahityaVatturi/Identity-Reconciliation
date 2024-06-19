fc80b703d9f1c97848dae7f089b392952eae0360
SahithyaVatturi <sahithyavatturi28@gmail.com>
2024-06-19 14:55:39 +0530

    changes in deployment

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const bodyParser = require("body-parser");
-const swaggerJSDoc = require("swagger-jsdoc");
-const swaggerUi = require("swagger-ui-express");
 const identityRoute = require("./src/routes/identity");
 const { connectDB } = require("./src/database/connection");
 
@@ -23,48 +21,16 @@ app.use((req, res, next) => {
 try {
   app.use("/identity-reconciliation/v1", identityRoute);
 
-  const swaggerDefinition = {
-    info: {
-      title: "Identity Reconciliation",
-      version: "1.0.0",
-      description: "Identity Reconciliation",
-    },
-    host: `localhost:${process.env.PORT}`,
-    basePath: "/identity-reconciliation/v1",
-  };
-
-  const options = {
-    swaggerDefinition: swaggerDefinition,
-    apis: ["./Controllers/**/*.js", "./api.yaml"],
-  };
-
-  const swaggerSpec = swaggerJSDoc(options);
-
   app.get("/", (req, res) => {
     res.status(200).send("Server is running");
   });
 
-  app.get("/swagger.json", (req, res) => {
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.send(swaggerSpec);
-  });
-
-  app.use("/apiDocs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-  app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-  });
+  // app.listen(process.env.PORT, () => {
+  //   console.log(`Server running on port ${process.env.PORT}`);
+  // });
 } catch (error) {
   console.log(error);
   throw new Error(error);
 }
 
-const onClose = () => {
-  process.exit();
-};
-
-//Handle process server.
-process.on("SIGTERM", onClose);
-process.on("SIGINT", onClose);
-process.on("uncaughtException", onClose);
+module.exports = app;
